399f6cd8e0872fae55c6af10a08c37fffce8159f
NIA-Schenk <nia.schenk@student.avans.nl>
2025-02-27 23:51:40 +0100

    Removed background script

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "toggle-synopsis") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "toggleSynopsis" });
-    });
-  }
-});
